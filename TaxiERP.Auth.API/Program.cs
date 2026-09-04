551247a77e911f32803173075eed2cbdf1de7507
Gabrielbrta <vdvsa159946@gmail.com>
2026-09-03 22:42:39 -0300

    feat: adicionado o Validation behavior para tratamento de exceções

--- TaxiERP.Auth.API/Program.cs
+++ TaxiERP.Auth.API/Program.cs
@@ -5,6 +5,8 @@ using TaxiERP.Auth.Application.Features.Auth.Commands.RegistrarOrganizacao;
 using TaxiERP.Auth.Domain.Interfaces;
 using TaxiERP.Auth.Infrastructure.Data;
 using TaxiERP.Auth.Infrastructure.Repositories;
+using MediatR;
+using TaxiERP.Auth.Application.Common.Behaviors;
 
 Env.Load();
 
@@ -14,8 +16,12 @@ var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
 
 //MediatR
 builder.Services.AddMediatR(cfg =>
-    cfg.RegisterServicesFromAssembly(typeof(RegistrarOrganizacaoCommand).Assembly)
-);
+{
+    cfg.RegisterServicesFromAssembly(typeof(RegistrarOrganizacaoCommand).Assembly);
+
+    cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+});
+
 
 // FluentValidation
 builder.Services.AddValidatorsFromAssembly(typeof(RegistrarOrganizacaoCommand).Assembly);
@@ -27,6 +33,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(conn
 //repositories
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<IOrganizacaoRepository, OrganizacaoRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
